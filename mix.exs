95e99863cd7312d17d6655fb4d4910e46e1c098b
Michael Truog <mjtruog@protonmail.com>
2021-12-04 14:05:18 -0800

    Version 2.0.4.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule CloudIServiceFunnel do
 
   def project do
     [app: :cloudi_service_funnel,
-     version: "2.0.3",
+     version: "2.0.4",
      language: :erlang,
      erlc_options: [
        :debug_info,
@@ -29,7 +29,7 @@ defmodule CloudIServiceFunnel do
   end
 
   defp deps do
-    [{:cloudi_core, "~> 2.0.3", app: false}]
+    [{:cloudi_core, ">= 2.0.4"}]
   end
 
   defp description do
